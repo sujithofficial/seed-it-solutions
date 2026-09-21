2a37c3976df9d60658eed1d20f92bded7c644083
Sujith <xsujithofficial@gmail.com>
2026-09-21 21:03:53 +0530

    Solve: [Q0.5] Basic_level_0_Datatypes_5 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-5-basic-level-0-datatypes-5/solution.py
@@ -0,0 +1,14 @@
+"""
+Problem: Q0.5 - Basic_level_0_Datatypes_5
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-21
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+# your code goes here
+
+value=input()
+print(value)
