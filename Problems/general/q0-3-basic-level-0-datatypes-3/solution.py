8ae17e6346fe2bf4007a2abb6288e730988b99f9
Sujith <xsujithofficial@gmail.com>
2026-09-21 21:01:32 +0530

    Solve: [Q0.3] Basic_level_0_Datatypes_3 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-3-basic-level-0-datatypes-3/solution.py
@@ -0,0 +1,12 @@
+"""
+Problem: Q0.3 - Basic_level_0_Datatypes_3
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-21
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+# your code goes here
+print(str("%%"))
